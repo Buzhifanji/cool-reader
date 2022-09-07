15f1dc5e10d887d743fcb3b194076699939bc085
huangbin <1127779503@qq.com>
2022-09-07 19:42:25 +0800

    新增 获取pdf封面方法

--- src/core/pdf.ts
+++ src/core/pdf.ts
@@ -1,9 +1,10 @@
+import { pdfLoadingTask } from "./utils";
+
 export function loadPdf(fileContent: Uint8Array) {
   const pdfjsLib = (window as Window)["pdfjs-dist/build/pdf"];
   const loadingTask = pdfjsLib.getDocument({ data: fileContent });
   loadingTask.promise
     .then((pdf: any) => {
-      console.log("PDF loaded", pdf);
       return pdf.getPage(1);
     })
     .then((page: any) => {
@@ -27,4 +28,29 @@ export function loadPdf(fileContent: Uint8Array) {
     .catch((error: any) => {
       console.log("error", error);
     });
+}
+
+export function getPDFCover(fileContent: Uint8Array): Promise<string> {
+  return new Promise<string>((resolve) => {
+    pdfLoadingTask(fileContent)
+      .promise.then((pdfDoc: any) => {
+        return pdfDoc.getPage(1);
+      })
+      .then((page: any) => {
+        const scale = 1.5;
+        const viewport = page.getViewport({ scale });
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        canvas.height = viewport.height || viewport.viewBox[3];
+        canvas.width = viewport.width || viewport.viewBox[2];
+        const taks = page.render({ canvasContext: context, viewport });
+        taks.promise.then(async () => {
+          const cover = canvas.toDataURL("image/jpeg");
+          resolve(cover);
+        });
+      })
+      .catch((err: any) => {
+        resolve("");
+      });
+  });
 }
