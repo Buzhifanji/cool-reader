b81a760f987bd75c444fd00b051833607c7ef282
huangbin <1127779503@qq.com>
2022-09-07 12:32:41 +0800

    新增 生成书本id方法

---
+++ src/core/md5.ts
@@ -0,0 +1,29 @@
+import { Bookextname } from "./type";
+import { pdfLoadingTask } from "./utils";
+
+/**
+ * 生成书本的唯一id
+ */
+export function setBookId(
+  file: File,
+  extname: string,
+  fileContent: Uint8Array
+) {
+  return new Promise<string>((resolve, reject) => {
+    try {
+      if (extname === Bookextname.pdf) {
+        pdfLoadingTask(fileContent)
+          .then((pdfDoc) => {
+            resolve(pdfDoc.fingerprints());
+          })
+          .catch((err: any) => {
+            resolve(file.name + "-" + file.size);
+          });
+      } else {
+        // TODO:
+      }
+    } catch (error) {
+      reject("set book id is error: " + error);
+    }
+  });
+}
