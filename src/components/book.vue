49b66e1912767e5277c713bb80f4e9490125eaa4
huangbin <1127779503@qq.com>
2022-09-07 12:35:09 +0800

    书本列表的初步demo

--- src/components/book.vue
+++ src/components/book.vue
@@ -1,16 +1,151 @@
 <template>
-  <div>book</div>
-  <button @click="action">测试</button>
-  <div>{{ value }}</div>
+  <n-card hoverable v-for="item in books" @click="openBook(item)">
+    <template #cover>
+      <n-image src="src/assets/2.jpg" preview-disabled />
+    </template>
+    <n-ellipsis :line-clamp="1">
+      {{ item.bookName }}
+    </n-ellipsis>
+  </n-card>
 </template>
 
 <script setup lang="ts">
-import { invoke } from '@tauri-apps/api/tauri';
-import { ref } from 'vue';
-const value = ref<string>('aa')
-async function action() {
-  const result = (await invoke('hello')) as string
-  value.value = result
-  const temp = (await invoke('readfile', { path: 'home' })) as string
-}
+import { NCard, NEllipsis, NImage } from "naive-ui";
+import { ref } from "vue";
+import { BaseBookType } from "../core/type";
+
+const books = ref<BaseBookType[]>([
+  {
+    bookName: "name1name1name1name1name1name1name1name1name1",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "1",
+  },
+  {
+    bookName: "name2",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "2",
+  },
+  {
+    bookName: "name3",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "3",
+  },
+  {
+    bookName: "name1name1name1name1name1name1name1name1name1",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "1",
+  },
+  {
+    bookName: "name2",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "2",
+  },
+  {
+    bookName: "name3",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "3",
+  },
+  {
+    bookName: "name1name1name1name1name1name1name1name1name1",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "1",
+  },
+  {
+    bookName: "name2",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "2",
+  },
+  {
+    bookName: "name3",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "3",
+  },
+  {
+    bookName: "name1name1name1name1name1name1name1name1name1",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "1",
+  },
+  {
+    bookName: "name2",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "2",
+  },
+  {
+    bookName: "name3",
+    extname: "pdf",
+    author: "佚名",
+    category: "",
+    size: 100,
+    path: "home",
+    content: [],
+    id: "3",
+  },
+]);
+
+function openBook(book: BaseBookType) {}
 </script>
+
+<style scoped>
+.n-card {
+  display: inline-flex;
+  width: 180px;
+  margin-right: 20px;
+  margin-top: 20px;
+  cursor: pointer;
+}
+</style>
