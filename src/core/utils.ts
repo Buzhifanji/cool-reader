2f7796666fd351a7e8630bc5515a19c0e4d6baa6
huangbin <1127779503@qq.com>
2022-09-07 19:43:35 +0800

    解决 getDocument 不是function的bug

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,4 +1,4 @@
-export const pdfLoadingTask = (fileContent: Uint8Array): Promise<any> => {
+export const pdfLoadingTask = (fileContent: Uint8Array): any => {
   const pdfjsLib = (window as Window)["pdfjs-dist/build/pdf"];
   return pdfjsLib.getDocument({ data: fileContent });
 };
