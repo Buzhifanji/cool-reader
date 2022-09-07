9cf0aa68b894d984f7f09608e9a72206a2c1c6fd
huangbin <1127779503@qq.com>
2022-09-07 19:46:10 +0800

    修改book 接口类型

--- src/core/type.ts
+++ src/core/type.ts
@@ -5,13 +5,16 @@ export const enum Bookextname {
   pdf = "pdf",
 }
 
-export interface BaseBookType {
+export interface BaseBook {
   bookName: string; // 书名
   extname: string; // 书本文件格式类型，例如 pdf、azw3
+  size: number; //  书本大小
+  id: string; // md5
+  lastModified?: number; // 最后修改时间
+}
+
+export interface bookType extends BaseBook {
   author: string; // 作者
   category: string; // 类别，用于书籍分类
-  size: number; //  书本大小
-  path: string; // 书本地址
-  content: any[]; // 内容
-  id: string; // md5
+  cover: string; // 封面
 }
