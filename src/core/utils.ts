44ea8a724864f65d8f685b4ea5dd669cbcb92f43
huangbin <1127779503@qq.com>
2022-09-15 21:24:40 +0800

    新增 mergerUint8Array 公共方法

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,4 +1,23 @@
-export const pdfLoadingTask = (fileContent: Uint8Array): any => {
+export const pdfLoadingTask = (fileContent: Uint8Array | string): any => {
   const pdfjsLib = (window as Window)["pdfjs-dist/build/pdf"];
-  return pdfjsLib.getDocument({ data: fileContent });
+  const param =
+    typeof pdfjsLib === "string" ? fileContent : { data: fileContent };
+  return pdfjsLib.getDocument(param);
 };
+
+/**
+ * 合并两个 Uint8Array，合并之后的顺序结果是： [...buf1, ...buf2]
+ * @param buf1
+ * @param buf2
+ * @returns
+ */
+export function mergerUint8Array(
+  buf1: Uint8Array,
+  buf2: Uint8Array
+): Uint8Array {
+  const n = buf1.length + buf2.length;
+  let result = new Uint8Array(n);
+  result.set(buf1);
+  result.set(buf2, buf1.length);
+  return result;
+}
