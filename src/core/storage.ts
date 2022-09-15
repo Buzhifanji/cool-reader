a3c89f82415554f581d1961014b623140146d76f
huangbin <1127779503@qq.com>
2022-09-15 22:04:04 +0800

    storage 迁出单独文件

---
+++ src/core/storage.ts
@@ -0,0 +1,37 @@
+import { open } from "@tauri-apps/api/dialog";
+import { downloadDir } from "@tauri-apps/api/path";
+import { forage } from "@tauri-apps/tauri-forage";
+import { books } from "./book";
+
+/**
+ * 本地缓存
+ */
+
+const storageLoction = "_tauri_storage_loction";
+
+export const bookStore = forage.createInstance({ name: "_cool_reader_" });
+
+export function clearStore() {
+  bookStore.clear();
+  books.value = [];
+}
+
+export async function changeStorageLoction() {
+  const selectedPath = await open({
+    directory: true,
+    multiple: false,
+    defaultPath: await getStoreageLoction(),
+  });
+  if (selectedPath) {
+    await forage.setItem({
+      key: storageLoction,
+      value: selectedPath as string,
+    });
+  }
+}
+
+export async function getStoreageLoction(): Promise<string> {
+  const param = { key: storageLoction };
+  const isExite = await forage.hasKey(param)();
+  return isExite ? await forage.getItem(param)() : await downloadDir();
+}
