279009c9898cc6d8160ef93f6f7df42c5f37e99d
huangbin <1127779503@qq.com>
2022-09-26 19:07:53 +0800

    修改 pdf 缓存信息

--- src/core/store/pdf.ts
+++ src/core/store/pdf.ts
@@ -2,18 +2,17 @@ import { PDFDocumentProxy } from "pdfjs-dist";
 
 interface CachePDF {
   document: PDFDocumentProxy;
-  height: number;
+  heights: Map<number, number>;
 }
 
-const pdfDocument = new Map<string, CachePDF>();
+const pdfDocument = new Map<string, CachePDF>(); // 缓存渲染 pdf 的 PDFDocumentProxy
 
 export function getPdfDocument(bookId: string) {
   const pdf = pdfDocument.get(bookId);
   if (pdf) {
     return {
       pdf: pdf.document,
-      numPages: pdf.document.numPages,
-      height: pdf.height,
+      heights: pdf.heights,
     };
   } else {
     return null;
@@ -22,4 +21,13 @@ export function getPdfDocument(bookId: string) {
 
 export function setPdfDocument(bookId: string, value: CachePDF) {
   return pdfDocument.set(bookId, value);
+}
+
+export function setPDfPagesHeight(
+  bookId: string,
+  index: number,
+  height: number
+) {
+  const pdf = pdfDocument.get(bookId);
+  pdf?.heights.set(index, height);
 }
