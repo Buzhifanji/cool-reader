d6a4a45d235cbf7ced76269bbd1ed97878cc1b9f
huangbin <1127779503@qq.com>
2022-10-04 02:07:34 +0800

    添加文本省略

--- src/components/highlight/highlight.vue
+++ src/components/highlight/highlight.vue
@@ -1,7 +1,9 @@
 <template>
   <n-list hoverable clickable>
     <n-list-item v-for="item in highlights" :key="item.id">
-      {{ item.text }}
+      <n-ellipsis :line-clamp="2">
+        {{ item.text }}
+      </n-ellipsis>
     </n-list-item>
   </n-list>
 </template>
