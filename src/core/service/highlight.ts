ba946fd203ab455d22d578e0fbd10895ed8899ae
huangbin <1127779503@qq.com>
2022-10-06 19:43:00 +0800

    添加 高亮笔记排序

--- src/core/service/highlight.ts
+++ src/core/service/highlight.ts
@@ -3,6 +3,24 @@ import { message } from "../../naive";
 import { highlightParam, highlightResponse } from "../notes/type";
 import { generateServiceParams } from "../utils/change-name";
 
+function sortHighlights(arr: highlightParam[]) {
+  return [...arr].sort((a, b) => {
+    if (a.pageNumber > b.pageNumber) {
+      return 1;
+    } else if (a.pageNumber < b.pageNumber) {
+      return -1;
+    } else {
+      if (a.startMeta.parentIndex > b.startMeta.parentIndex) {
+        return 1;
+      } else if (a.startMeta.parentIndex < b.startMeta.parentIndex) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+}
+
 export function saveHighlight(param: highlightParam) {
   const data = generateServiceParams<highlightParam, highlightResponse>(param);
   invoke("add_highlight", { data })
@@ -21,7 +39,8 @@ export function getHighlights(bookId: string): Promise<highlightParam[]> {
         const result = (value as highlightResponse[]).map((item) =>
           generateServiceParams<highlightResponse, highlightParam>(item, false)
         );
-        resolve(result);
+        console.log("result", sortHighlights(result));
+        resolve(sortHighlights(result));
       })
       .catch((err) => {
         message.error(err);
