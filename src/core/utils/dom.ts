92e9c57d1068fbc39254214472b08bedc5aa558e
huangbin <1127779503@qq.com>
2022-10-09 12:47:16 +0800

    迁移 dom 工具函数

---
+++ src/core/utils/dom.ts
@@ -0,0 +1,26 @@
+import { NIcon } from "naive-ui";
+import { Component, h } from "vue";
+
+export function createEle(tagName: string) {
+  return document.createElement(tagName);
+}
+
+export function getEleById(id: string) {
+  return document.getElementById(id);
+}
+
+export function selectorAll(selectors: string, doc?: HTMLElement) {
+  const $root = doc || document;
+  return $root.querySelectorAll<HTMLElement>(selectors);
+}
+
+export function selector(selector: string, doc?: HTMLElement) {
+  const $root = doc || document;
+  return $root.querySelector<HTMLElement>(selector);
+}
+
+export const renderIcon = (icon: Component) => () =>
+  h(NIcon, null, { default: () => h(icon) });
+
+export const getPDFPageSelector = (pageNumber: number) =>
+  `div.page[data-page-number="${pageNumber}"] .textLayer`;
