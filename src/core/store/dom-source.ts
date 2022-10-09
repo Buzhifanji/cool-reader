2ecf4a6176c71ffbf143dd29925d13ed72574472
huangbin <1127779503@qq.com>
2022-10-09 13:01:07 +0800

    添加 文本内容存储

---
+++ src/core/store/dom-source.ts
@@ -0,0 +1,23 @@
+import { DomSource } from "../toolbar/type";
+
+const data: Map<string, DomSource> = new Map();
+
+export function saveDomSource(source: DomSource | DomSource[]) {
+  if (Array.isArray(source)) {
+    source.forEach((s) => data.set(s.id, s));
+  } else {
+    data.set(source.id, source);
+  }
+}
+
+export function removeDomSource(id: string) {
+  return data.delete(id);
+}
+
+export function getDomSource(id: string) {
+  return data.get(id);
+}
+
+export function hasDomSource(id: string) {
+  return data.has(id);
+}
