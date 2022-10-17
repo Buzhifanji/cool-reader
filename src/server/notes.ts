ef5b2c993170d586ce5822954426429d2280664f
huangbin <1127779503@qq.com>
2022-10-17 20:58:30 +0800

    添加 前端笔记 接口

---
+++ src/server/notes.ts
@@ -0,0 +1,19 @@
+import { DomSource } from "@/interfaces";
+import { changeNotes, deleteNotes, _getNotes } from "@/utils";
+
+export function saveNotes(param: DomSource) {
+  return changeNotes(param, "add_notes", "添加成功");
+}
+
+export function updateNotes(param: DomSource) {
+  return changeNotes(param, "update_notes", "修改成功");
+}
+
+export function getHighlights(bookId: string): Promise<DomSource[]> {
+  return _getNotes(bookId, "get_notes");
+}
+
+export function removeHighlight(book_id: string, id: string, isTip = true) {
+  const data = { book_id, id };
+  return deleteNotes(data, "delete_notes", isTip);
+}
