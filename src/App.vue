eed1a493af9f5fc90f386cc8fc8fd0860024df53
huangbin <1127779503@qq.com>
2022-11-09 19:02:37 +0800

    修改 ui根据浏览器语言加载相对于的语言

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { naiveUiLanguage } from "@i18n/index";
+
 import { lightTheme } from "naive-ui";
 </script>
 
 <template>
-  <n-config-provider :theme="lightTheme">
+  <n-config-provider :theme="lightTheme" :locale="naiveUiLanguage.locale" :date-locale="naiveUiLanguage.date">
     <router-view></router-view>
   </n-config-provider>
 </template>
