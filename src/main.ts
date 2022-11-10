68b1b7e500a4909558562ca4d59ab83c1c445185
huangbin <1127779503@qq.com>
2022-11-10 17:12:17 +0800

    优化开发体验

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { naive } from "./naive";
 import { router } from "./route";
+import devtools from "@vue/devtools";
+
+if (process.env.NODE_ENV === "development") {
+  devtools.connect("http://localhost", 8098);
+}
 
 GlobalWorkerOptions.workerSrc =
   "../node_modules/pdfjs-dist/build/pdf.worker.js";
