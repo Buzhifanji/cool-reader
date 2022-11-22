c6973212f84b4e98d11261470863cfdfa50cc6bf
buzhifanji <buzhifanji@163.com>
2022-11-22 13:46:29 +0800

    创建 输入笔记组件

---
+++ src/components/idea-input/index.ts
@@ -0,0 +1,74 @@
+import { createDiscreteApi, MessageReactive } from "naive-ui";
+import { createUUID, DomSource } from "src/core/web-highlight";
+import { message } from "src/naive";
+import { saveNotes, updateNotes } from "src/server/notes";
+import { getReadingBook } from "src/store";
+import { createTime } from "src/utils";
+import { getIdeas } from "../idea/idea";
+import Input from './index.vue'
+
+let messageReactive: MessageReactive | null = null;
+let source: DomSource | null = null;
+let isEdit: boolean = false
+
+const ideas = ref<string[]>([]);
+
+export function removeMessage() {
+  if (messageReactive) {
+    messageReactive.destroy();
+    messageReactive = null;
+  }
+}
+
+export function openIdea(_source: DomSource, _isEdit: boolean) {
+  source = _source;
+  isEdit = _isEdit;
+
+  const { message } = createDiscreteApi(["message"], {
+    messageProviderProps: {
+      placement: "bottom",
+      containerStyle: { bottom: "50px" },
+    },
+  });
+
+  removeMessage();
+
+  messageReactive = message.create("", {
+    render: () => h(Input),
+    closable: true,
+    duration: 0,
+    showIcon: false,
+  });
+}
+
+export const useInputIdea = () => {
+  const text = ref<string>("");
+  async function submit() {
+    const content = unref(text);
+    if (content) {
+      debugger
+      if (source) {
+        source.notes = { id: createUUID(), tag: '', createTime: createTime(), content }
+
+        if (isEdit) {
+          await updateNotes(source)
+        } else {
+          const readingBook = getReadingBook();
+          source.bookId = readingBook.id;
+
+          await saveNotes(source)
+        }
+        getIdeas()
+      }
+    } else {
+      message.error("您还未填写笔记！");
+    }
+  }
+  return { text, submit };
+};
+
+export const generatoNotes = (source: DomSource) => {
+  if (source.notes) {
+
+  }
+}
---
+++ src/components/idea-input/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useInputIdea } from ".";
+const { text, submit } = useInputIdea()
+
+</script>
+<template>
+  <div class="idea-container">
+    <NInput v-model:value="text" class="idea-input" type="textarea" placeholder="输入你的想法" clearable round show-count
+      size="large" rows="6" maxlength="999" />
+    <div class="idea-button-container">
+      <NButton @click="submit" type="primary" round>添加</NButton>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.idea-input {
+  max-height: 600px;
+  margin-bottom: 10px;
+}
+
+.idea-button-container {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
