91e2627a6034f31e15e0df039042c6102ae5b1b1
buzhifanji <buzhifanji@163.com>
2022-11-25 09:56:49 +0800

    删除 getTextWidth 函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,6 @@ export * from "./catalog";
 export * from "./dom";
 export * from "./fied";
 export * from "./is";
-export * from "./text";
 export * from "./union";
 export * from "./time"
 export * from "./doc"
