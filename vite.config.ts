cc0aafa638c371440faca6512b15dfdb976160d7
buzhifanji <buzhifanji@163.com>
2022-12-07 16:04:46 +0800

    添加 web-component 配置

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,16 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(
+      {
+        template: {
+          compilerOptions: {
+            // 将所有带短横线的标签名都视为自定义元素
+            isCustomElement: (tag) => tag.includes('virtual-list')
+          }
+        }
+      }
+    ),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
