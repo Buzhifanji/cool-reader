fa64e2a13ab1a9f31617d8ff823516632e213936
buzhifanji <buzhifanji@163.com>
2022-12-07 16:05:58 +0800

    修改 我的高亮组件

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,10 +3,11 @@ import { NotesSource } from "src/interfaces/components/notes";
 
 export const createTime = () => new Date().valueOf();
 
-export const hanldeList = (list: DomSource[]) => {
-  const map = new Map<number, DomSource[]>();
+export const hanldeNotesByTime = (list: DomSource[]) => {
+  // 按时间分类整理
+  const map = new Map<string, DomSource[]>();
   list.forEach(item => {
-    const time = item.createTime
+    const time = useDateFormat(item.createTime, 'YYYY-MM-DD').value
     const content = map.get(time)
     if (content) {
       content.push(item)
@@ -15,10 +16,20 @@ export const hanldeList = (list: DomSource[]) => {
     }
   })
 
-  const result: NotesSource[] = []
+  // 按时时间排序
+  const sortList: { time: string, content: DomSource[] }[] = []
   map.forEach((value, key) => {
-    result.push({ time: key, content: value })
+    sortList.push({ time: key, content: value })
   })
+  sortList.sort((a, b) => a.time < b.time ? 1 : -1)
 
-  return result.sort((a, b) => a.time < b.time ? -1 : 1)
+  // 转换成数组（方便实现虚拟列表功能）
+  const result: NotesSource[] = []
+  sortList.forEach(({ time, content }) => {
+    result.push({ time, id: time })
+    content.forEach(value => {
+      result.push({ ...value, id: value.createTime.toString() })
+    })
+  })
+  return result
 }
--- src/views/highlight/highlight.vue
+++ src/views/highlight/highlight.vue
@@ -1,23 +1,56 @@
 <script setup lang="ts">
-import { Search, ShowDataCards, List } from "@vicons/carbon";
+import { Search, ShowDataCards, Delete, List } from "@vicons/carbon";
 import { langField } from 'src/i18n';
-import { NotesSource } from "src/interfaces/components/notes";
+import { NotesSource } from "src/interfaces";
 import { getAllNotes } from "src/server/notes";
-import { hanldeList } from "src/utils";
+import { hanldeNotesByTime } from "src/utils";
+import { initRenderData, visiableData } from "render-big-data";
+
 
 const total = ref<number>(0)
-const list = ref<NotesSource[]>([])
+
+// 目前不知道 如何在HTML配置 // @ts-ignore，所以使用了 any类型
+const list = ref<any[]>([])
+
+// const { visibleList, initHeight, updateHeight, watchScroll, scroll } = useVirtualList<NotesSource>(list)
+
+// const contentStyle = computed(() => {
+//   return {
+//     padding: '24px',
+//     height: `${scroll.scrollBarHeight}px`
+//   }
+// })
+
+// const transform = computed(() => {
+//   return { transform: `translate3d(0, ${scroll.scrollTop}px), 0` }
+// })
 
 getAllNotes().then(value => {
   total.value = value.length
-  list.value = hanldeList(value)
+  const arr = hanldeNotesByTime(value)
+  initRenderData(arr, {
+    renderNum: 20
+  })
+  // initHeight()
 })
+
+const unsubscribe = visiableData.subscribe((value: any) => (list.value = value));
+
+function update() {
+
+}
+
+function onScroll(e: Event) {
+  console.log('-------------')
+  const target = e.target as HTMLElement;
+}
+
 
 </script>
 
 <template>
   <n-layout style="height: 100vh; padding: 0 24px;">
-    <n-layout-header style="height: 60px; padding: 4px">
+    <n-layout-header style="height: 60px; padding: 4px" bordered>
       <n-space justify="space-between">
         <n-statistic :label="langField.highlightRecordPrev" tabular-nums>
           <n-number-animation ref="numberAnimationInstRef" :from="0" :to="total" />
@@ -52,13 +85,33 @@ getAllNotes().then(value => {
 
     </n-layout-header>
     <n-layout position="absolute" style="top: 60px; bottom: 0px" has-sider>
-      <n-layout content-style="padding: 24px;" :native-scrollbar="false">
-        <template v-for="item in list" :key="item.time">
-          <n-p>
-            <n-time :time="item.time" format="yyyy-MM-dd" />
-          </n-p>
-          <n-card v-for="sub in item.content">{{ sub.text }}</n-card>
-        </template>
+      <n-layout :native-scrollbar="false" contentStyle="height: 100%" @onScroll="onScroll">
+        <virtual-list>
+          <virtual-list-item v-for="(item, index) in list" :index="index" :key="item.time">
+            <n-h1 v-if="item.time">{{ item.time }} </n-h1>
+            <div class="text" v-else>
+              <n-p depth="4" style="--n-font-size: 16px;">
+                {{ item.text }}
+              </n-p>
+              <n-divider />
+            </div>
+
+            <!-- <n-p v-if="item.time">{{ item.time }} </n-p> -->
+            <!-- <n-card v-else>
+              {{ item.text }}
+              <template #action>
+                <n-space justify="end" align="center">
+                  <n-button text circle :bordered="false" style="font-size: 20px">
+                    <n-icon :component="Delete" />
+                  </n-button>
+                  <n-button text tag="a" href="https://anyway.fm/news.php" target="_blank" type="primary">
+                    来自：{{ item.bookId }}
+                  </n-button>
+                </n-space>
+              </template>
+            </n-card> -->
+          </virtual-list-item>
+        </virtual-list>
       </n-layout>
     </n-layout>
   </n-layout>
@@ -74,7 +127,37 @@ getAllNotes().then(value => {
   margin-bottom: 10px;
 }
 
+.n-h1,
+.text {
+  text-align: left;
+  cursor: pointer;
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+.n-h1 {
+  padding-top: 20px;
+}
+
+.n-divider {
+  margin: 0;
+}
+
+.n-p {
+  padding: 20px 0px;
+  margin: 0;
+}
+
 .n-button {
   box-shadow: none;
+}
+
+.n-card:deep(.n-card__action) {
+  padding: 5px 20px;
+}
+
+.scroller {
+  height: 100%;
+  overflow-y: scroll;
 }
 </style>
